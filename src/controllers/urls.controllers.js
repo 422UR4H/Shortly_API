7ca7a9fd0a4506be7ec0e17292e40e1fb2dc1f8a
422UR4H <tiagosindra@id.uff.br>
2023-08-01 02:27:56 -0300

    :construction: feat/openUrl

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -1,13 +1,25 @@
-export default async function shorten(req, res) {
+export async function shorten(req, res) {
     res.sendStatus(501); // 201
 }
 
-export default async function getUrlById(req, res) {
+export async function getUrlById(req, res) {
     const { id } = req.params;
 
     try {
-        
+
         res.sendStatus(501); // 200
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
+
+export async function openUrl(req, res) {
+    const { shortUrl } = req.params;
+    if (!shortUrl) return res.sendStatus(404);
+
+    try {
+
+        res.sendStatus(501); // res.redirect
     } catch (err) {
         res.status(500).send(err.message);
     }
--- src/routers/urls.routes.js
+++ src/routers/urls.routes.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 import { urlsSchema } from "../schemas/urls.schemas.js";
 import validateSchema from "../middlewares/validateSchema.js";
 import validateAuth from "../middlewares/validateAuth.js";
-import shorten from "../controllers/urls.controllers.js";
+import { getUrlById, shorten } from "../controllers/urls.controllers.js";
 
 
 const router = Router();
 
 router.post("/urls/shorten", validateAuth(""), validateSchema(urlsSchema), shorten);
+router.get("/urls/:id", getUrlById);
 
 export default router;
