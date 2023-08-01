e1e8a943e5ec62cff3e58e134611ca3c510baa74
422UR4H <tiagosindra@id.uff.br>
2023-08-01 02:22:22 -0300

    :construction: feat/getUrlById

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -1,3 +1,14 @@
 export default async function shorten(req, res) {
-    res.sendStatus(501);
+    res.sendStatus(501); // 201
+}
+
+export default async function getUrlById(req, res) {
+    const { id } = req.params;
+
+    try {
+        
+        res.sendStatus(501); // 200
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 }
