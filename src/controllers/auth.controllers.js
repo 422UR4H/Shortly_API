cfa322d1bb3a598d2f4021600fd34cb459509a4a
422UR4H <tiagosindra@id.uff.br>
2023-08-01 17:41:24 -0300

    :construction: feat/auth

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,5 +1,19 @@
 export async function signup(req, res) {
-    res.sendStatus(501);
+    const { name, email, password, confirmPassword } = req.body;
+    if (password !== confirmPassword) {
+        return res.status(422).send("As senhas não conferem!");
+    }
+
+    try {
+        // check if email exists in db
+        //res.status(409).send("E-mail já cadastrado!");
+
+        // insert user in db
+
+        res.sendStatus(201);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 }
 
 export async function getUser(req, res) {
--- src/middlewares/validateAuth.js
+++ src/middlewares/validateAuth.js
@@ -1,6 +1,12 @@
-export default function validateAuth(auth) {
-    return (req, res, next) => {
-        
-        next();
-    };
+export default function validateAuth(req, res, next) {
+    const { authorization } = req.headers;
+    const token = authorization?.replace("Bearer ", "");
+    if (!token) return res.sendStatus(401);
+
+    try {
+        // get sessions?
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+    next();
 }
