4c623a6527f7acef0518fb8e65870da16683ead1
422UR4H <tiagosindra@id.uff.br>
2023-08-01 01:45:17 -0300

    :construction: architecture

---
+++ src/middlewares/validateAuth.js
@@ -0,0 +1,6 @@
+export default function validateAuth(auth) {
+    return (req, res, next) => {
+        
+        next();
+    };
+}
---
+++ src/routers/urls.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import validateSchema from "../middlewares/validateSchema.js";
+
+
+const router = Router();
+
+
+
+export default router;
