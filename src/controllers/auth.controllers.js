8138ef6e6d683222753473df493ae41edc4353f8
422UR4H <tiagosindra@id.uff.br>
2023-08-01 02:39:08 -0300

    :construction: feat/getUser

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,3 +1,12 @@
-export default async function signup(req, res) {
+export async function signup(req, res) {
     res.sendStatus(501);
+}
+
+export async function getUser(req, res) {
+    try {
+
+        res.sendStatus(501); // 200
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 }
--- src/routers/auth.routes.js
+++ src/routers/auth.routes.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { usersSchema } from "../schemas/users.schemas.js";
 import validateSchema from "../middlewares/validateSchema.js";
-import signup from "../controllers/auth.controllers.js";
+import validateAuth from "../middlewares/validateAuth.js";
+import { signup, getUser } from "../controllers/auth.controllers.js";
 
 
 const router = Router();
 
 router.post("/signup", validateSchema(usersSchema), signup);
+// router.post("/signin", ..., signin);
+router.get("/users/me", validateAuth(""), getUser)
 
 export default router;
